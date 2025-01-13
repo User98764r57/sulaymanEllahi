f57e794caf1a688089993351c00079eae8e6ad6e
User98764r57 <163321186+User98764r57@users.noreply.github.com>
2025-01-13 05:41:28 +0000

    Update countPersonnelByDepartment.php

--- Project_2/libs/php/countPersonnelByDepartment.php
+++ Project_2/libs/php/countPersonnelByDepartment.php
@@ -8,7 +8,6 @@
 	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
 
 	if (mysqli_connect_errno()) {
-		
 		$output['status']['code'] = "300";
 		$output['status']['name'] = "failure";
 		$output['status']['description'] = "database unavailable";
@@ -20,47 +19,44 @@
 		echo json_encode($output);
 		
 		exit;
-
-	}	
+	} 
 
 	$query = $conn->prepare('SELECT firstName FROM personnel WHERE departmentID =  ?');
-
 	$query->bind_param("i", $_POST['departmentID']);
-
 	$query->execute();
-	
-	if (false === $query) {
 
+	if (false === $query) {
 		$output['status']['code'] = "400";
 		$output['status']['name'] = "executed";
-		$output['status']['description'] = "query failed";	
+		$output['status']['description'] = "query failed";
 		$output['data'] = [];
 
 		echo json_encode($output); 
-	
+		
 		mysqli_close($conn);
 		exit;
-
 	}
 
 	$result = $query->get_result();
 
-   	$data = [];
+    $data = [];
 
 	while ($row = mysqli_fetch_assoc($result)) {
-
 		array_push($data, $row);
-
 	}
 
+	$countQuery = $conn->prepare('SELECT COUNT(*) as count FROM personnel WHERE departmentID = ?');
+	$countQuery->bind_param("i", $_POST['departmentID']);
+	$countQuery->execute();
+	$countResult = $countQuery->get_result()->fetch_assoc();
 
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $data;
+	$output['data']['personnel'] = $data;
+	$output['data']['count'] = $countResult['count'];
 	
-
 	echo json_encode($output); 
 
 	mysqli_close($conn);
