b6819440e105acb5aa261f75964fac7177e6c854
User98764r57 <163321186+User98764r57@users.noreply.github.com>
2025-01-13 09:06:20 +0000

    Update deletePersonnelByID.php

--- Project_2/libs/php/deletePersonnelByID.php
+++ Project_2/libs/php/deletePersonnelByID.php
@@ -8,7 +8,6 @@
 	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
 
 	if (mysqli_connect_errno()) {
-		
 		$output['status']['code'] = "300";
 		$output['status']['name'] = "failure";
 		$output['status']['description'] = "database unavailable";
@@ -20,38 +19,35 @@
 		echo json_encode($output);
 
 		exit;
-
-	}	
+	} 
 
 	$query = $conn->prepare('DELETE FROM personnel WHERE id = ?');
-	
 	$query->bind_param("i", $_POST['id']);
-
 	$query->execute();
-	
-	if (false === $query) {
 
+	if (false === $query) {
 		$output['status']['code'] = "400";
 		$output['status']['name'] = "executed";
-		$output['status']['description'] = "query failed";	
+		$output['status']['description'] = "query failed";
 		$output['data'] = [];
 
 		mysqli_close($conn);
 
-		echo json_encode($output); 
+		echo json_encode($output);
 
 		exit;
-
 	}
+
+	$countQuery = $conn->query('SELECT COUNT(*) as count FROM personnel');
+	$countResult = $countQuery->fetch_assoc();
 
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = [];
-	
+	$output['data']['remaining'] = $countResult['count'];
+
 	mysqli_close($conn);
 
-	echo json_encode($output); 
-
+	echo json_encode($output);
 ?>
