37616b19abde8c2d9fc77cf44aa0f0e28fb855dc
User98764r57 <163321186+User98764r57@users.noreply.github.com>
2025-10-08 19:34:23 +0100

    Update fetch_weatherData.php

--- Project_1/libs/php/fetch_weatherData.php
+++ Project_1/libs/php/fetch_weatherData.php
@@ -3,46 +3,28 @@ header('Content-Type: application/json');
 
 $username = 'sulayman2e';
 
-// Enhanced list of ICAO codes covering more countries
 $icaoCodes = [
-    // North America
     'KATL', 'KLAX', 'KORD', 'KDFW', 'KDEN', 'KJFK', 'KLAS', 'KMIA', 'KSEA', 'KBOS',
     'KIAH', 'KMSP', 'KDTW', 'KPHX', 'KPHL', 'KSLC', 'KSAN', 'KTPA', 'KMCO', 'KPDX',
-    
-    // Canada
     'CYYZ', 'CYVR', 'CYUL', 'CYYC', 'CYEG', 'CYOW', 'CYWG', 'CYHZ', 'CYQB', 'CYQR',
-    
-    // Europe
     'EGLL', 'EHAM', 'EDDF', 'LFPG', 'LEMD', 'LIRF', 'LSZH', 'EBBR', 'ENGM', 'ESSA',
     'LOWW', 'EFHK', 'EIDW', 'BIRK', 'EINN', 'LPPT', 'LEBL', 'LZIB', 'LKPR', 'LDZA',
-    
-    // Asia
     'RJTT', 'RKSI', 'ZBAA', 'VIDP', 'VTBS', 'WSSS', 'RPLL', 'VHHH', 'WMKK', 'VABB',
     'ZSPD', 'ZGGG', 'ZPPP', 'ZUUU', 'ZBTJ', 'ZSHC', 'VCRK', 'VOCI', 'OPLA', 'OMDB',
-    
-    // Oceania
     'YSSY', 'NZAA', 'YPAD', 'YMML', 'YPPH', 'YBBN', 'YBCS', 'NZWN', 'NZCH', 'NFFN',
-    
-    // South America
     'SBGR', 'SCEL', 'SABE', 'SUMU', 'SPJC', 'SLLP', 'SKBO', 'SKRG', 'SBBR', 'SBCF',
-    
-    // Africa
     'FAOR', 'HKJK', 'DNAA', 'HECA', 'FACT', 'GMAD', 'DTTA', 'FKKD', 'HFFF', 'FBSK',
-    
-    // Middle East
     'OMDB', 'OTHH', 'OEJN', 'OJAI', 'OKBK', 'OLBA', 'OOMS', 'OIIE', 'OISS', 'OEDF'
 ];
 
 $weatherData = [];
 
-// Limit concurrent requests to prevent timeout
 $batchSize = 10;
 $batches = array_chunk($icaoCodes, $batchSize);
 
 foreach ($batches as $batch) {
     $responses = [];
     
-    // Create multi curl for batch processing
     $multiCurl = curl_multi_init();
     $curlHandles = [];
     
@@ -56,13 +38,11 @@ foreach ($batches as $batch) {
         $curlHandles[$icao] = $ch;
     }
     
-    // Execute the batch
     $running = null;
     do {
         curl_multi_exec($multiCurl, $running);
     } while ($running);
     
-    // Process responses
     foreach ($curlHandles as $icao => $ch) {
         $response = curl_multi_getcontent($ch);
         $data = json_decode($response, true);
@@ -86,9 +66,8 @@ foreach ($batches as $batch) {
     
     curl_multi_close($multiCurl);
     
-    // Small delay between batches to be respectful to the API
     if (count($batches) > 1) {
-        usleep(200000); // 0.2 seconds
+        usleep(200000);
     }
 }
 
