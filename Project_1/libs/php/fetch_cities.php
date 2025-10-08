af10b7d94f91582242c5b190a5fa86f8102bea54
User98764r57 <163321186+User98764r57@users.noreply.github.com>
2025-10-08 18:03:55 +0100

    Update fetch_cities.php

--- Project_1/libs/php/fetch_cities.php
+++ Project_1/libs/php/fetch_cities.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 
 if (isset($_GET['isoCode'])) {
-    $isoCode = $_GET['isoCode'];
+    $isoCode = $_GET['isoCode']; 
     $username = '(enter username/API Key)'; 
     $url = "http://api.geonames.org/searchJSON?country=$isoCode&maxRows=1000&username=$username";
 
