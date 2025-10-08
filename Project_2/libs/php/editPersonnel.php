4ea13e8b7ee5fea7d7ff246618cac8a957743d7d
User98764r57 <163321186+User98764r57@users.noreply.github.com>
2025-10-08 16:51:07 +0100

    Update editPersonnel.php

--- Project_2/libs/php/editPersonnel.php
+++ Project_2/libs/php/editPersonnel.php
@@ -3,7 +3,7 @@
 
 	include("config.php");
 
-	header('Content-Type: application/json; charset=UTF-8');
+	header('Content-Type: application/json; charset=UTF-8');   
 
 	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
 
